3ecaf0ba75f1bc90f64e5f5209cc2433d44fcdad
Your <edu.alri@gmail.com>
2024-02-13 22:42:20 -0300

    requirements intalled

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'cellSegmentation',
+    version= '0.0.0',
+    author= 'eduribeiro12',
+    author_email= 'edu.alri@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+)
